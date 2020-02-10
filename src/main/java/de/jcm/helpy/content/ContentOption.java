07673586791dd7217249d18478d1c2c36a34e992
JCM <jacama2009@gmail.com>
2020-02-10 13:56:35 +0100

    Add message option to ContentOption

--- src/main/java/de/jcm/helpy/content/ContentOption.java
+++ src/main/java/de/jcm/helpy/content/ContentOption.java
@@ -1,8 +1,12 @@
 package de.jcm.helpy.content;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ContentOption
 {
 	public int id;
 	public String[] answers;
 	public String target;
+	public String message;
 }
